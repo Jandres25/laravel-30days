4d4012aefe5e65028f6d83f54e8e11d53fcd9e7b
Jandres25 <sc.jose.meneces.l@upds.net.bo>
2026-05-06 16:48:15 -0400

    feat(day-07): add Job model, routes and views; handle missing job (404)
    Add app/Models/Job.php with all() and find() using Arr::first; find() aborts(404) when not found. Add routes for jobs index and show in routes/web.php.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,52 +9,13 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Developer',
-                'salary' => '$20,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Designer',
-                'salary' => '$10,000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Developer',
-            'salary' => '$20,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Designer',
-            'salary' => '$10,000'
-        ]
-    ];
-
     // ensure proper type comparison
-    $job = Arr::first($jobs, fn($job) => $job['id'] === (int) $id);
-
-    // If job not found, return 404
-    if (! $job) {
-        abort(404);
-    }
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
